6d5bff06fc340a205ea2fa51040019468b9333bb
timqian <timqian92@qq.com>
2017-03-19 10:13:31 +0800

    init

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+// hello.cpp
+
+#include <iostream>  // 定义了一些头文件，包含了程序中必需的或有用的信息
+using namespace std;  // 使用 std 命名空间
+ 
+int main() // main() 是程序开始执行的地方
+{
+   cout << "Hello World"; // 输出 Hello World
+   return 0;  // 终止 main( )函数，并向调用进程返回值 0
+}
